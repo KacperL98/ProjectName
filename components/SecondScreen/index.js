56c200e03b4532a34a3da6bc97c1a6a275e6b56f
KacperL98 <kacperlitwinow517@gmail.com>
2021-03-02 17:52:42 +0100

    test

--- components/SecondScreen/index.js
+++ components/SecondScreen/index.js
@@ -1,36 +1,36 @@
 import React from 'react';
-import {SafeAreaView, 
-  View, 
-  Text, KeyboardAvoidingView, ScrollView
+import {
+  SafeAreaView,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
 import styles from './styles';
-import _StyledButton from '../StyledButton'
+import _StyledButton from '../StyledButton';
 import _textResult from '../TextResults';
 
 const SecondPage = ({route, navigation}) => {
   return (
-
     <SafeAreaView style={{flex: 1}}>
       <ScrollView>
-      <View style={styles.container}>
-      <View style={styles.circle}/>
-      <Text style={styles.textLogo}>Logo</Text>
-     
-      <Text style={styles.textStyle}>
-          Aplikacja testowa</Text>
-        
-        <_textResult 
-        content={route.params.paramKey}
-        txtPreviousV={"Dane pobrane z pierwszego ekranu"}
-        />
-      </View>
-      <_StyledButton
+        <View style={styles.container}>
+          <View style={styles.circle} />
+          <Text style={styles.textLogo}>Logo</Text>
+
+          <Text style={styles.textStyle}>Aplikacja testowa</Text>
+
+          <_textResult
+            content={route.params.paramKey}
+            txtPreviousV={'Dane pobrane z pierwszego ekranu'}
+          />
+        </View>
+        <_StyledButton
           type="secondary"
-          content={"Wróć"}
-          onPress={() =>
-            navigation.navigate('FirstPage')}
+          content={'Wróć'}
+          onPress={() => navigation.navigate('FirstPage')}
         />
-        </ScrollView>
+      </ScrollView>
     </SafeAreaView>
   );
 };
